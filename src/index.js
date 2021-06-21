68759bbc8fe16b2136f33e5cd31da03ad4d3d6c9
TestadeNevill <64296826+TestadeNevill@users.noreply.github.com>
2021-06-21 16:42:59 -0400

    redux practice

--- src/index.js
+++ src/index.js
@@ -3,14 +3,27 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore, combineReducers } from 'redux';
+import allReducer from './reducers';
+import { Provider } from 'react-redux';
+
+
+const store = createStore(allReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+
+
+//STORE - Globalized State
+
+//ACTION  - INCREMENT Fucntion that returns object.
+
+
+
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
